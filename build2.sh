c50ed01093a5356ac1587df2f39355aa5479197e
Dasha6060 <d.mosulega@g.nsu.ru>
2025-09-25 21:40:40 +0700

    Create build2.sh

---
+++ build2.sh
@@ -0,0 +1,7 @@
+gcc -Wall -Wextra -Werror -Og lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -Ofast lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -Os lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -O3 lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -O2 lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -O1 lab_evm1.c -o lab_evm1.exe
+gcc -Wall -Wextra -Werror -O0 lab_evm1.c -o lab_evm1.exe
